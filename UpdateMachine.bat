c0b6c10493648b46cee3ecb2b6d0fd48dab52f0f
Marathe <darshan.marathe@philips.com>
2021-10-28 17:20:02 +0530

    added update machine command

---
+++ UpdateMachine.bat
@@ -0,0 +1,11 @@
+del ".UpdateMachine.bat.un~"
+del "UpdateMachine.bat~"
+choco list -lo
+pause
+choco upgrade all
+pause
+call npm list -g --depth=0
+pause
+call npm outdated -g --depth=0
+pause
+call npm update -g
