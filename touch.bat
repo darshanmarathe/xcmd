02d7b966f736a6c96de4e9a42b424359cbd3e0ef
Darshan Marathe <darshanmarathe@gmail.com>
2022-07-13 12:50:59 +0530

    touch now works

--- touch.bat
+++ touch.bat
@@ -6,8 +6,8 @@ IF %ERRORLEVEL% == 0 node %~dp0touch.js %* %cd% && EXIT /B 2
 
 
 
-@REM WHERE scriptcs
-@REM IF %ERRORLEVEL% == 0 scriptcs %~dp0touch.csx -C -- %* %cd% && EXIT /B 2
+WHERE scriptcs
+IF %ERRORLEVEL% == 0 scriptcs %~dp0touch.csx -C -- %* %cd% && EXIT /B 2
 
 
 
--- touch.js
+++ touch.js
@@ -1,4 +1,5 @@
-
+var fs = require('fs');
+ 
 
 
 const thingsToCreate  = process.argv;
@@ -13,27 +14,49 @@ function getDirPath(path ="") {
     if(path === "..")
         {
             let arr =directoryPath.split("\\")
-            arr.pop();
-            
+            if(arr[arr.length -1].indexOf(".") > -1)
+                arr.pop();
+     
+            arr.pop()
             directoryPath = arr.join("\\")
             return;
         }
-    if(path.indexOf(".") > -1)
-    {
+    // if(path.indexOf(".") > -1)
+    // {
         let arr =directoryPath.split("\\")
         if(arr[arr.length -1].indexOf(".") > -1)
             arr.pop();
         directoryPath = arr.join("\\")
        directoryPath=  directoryPath + "\\" + path;;
         return;
-    }
+    // }
+    
 
-    directoryPath =  directoryPath + "\\" + path; 
 }
 
 
+function createFile(path){
+ 
+// writeFile function with filename, content and callback function
+if(fs.existsSync(path) === false)    
+    fs.writeFileSync(path, '');
+}
+
+function createDir(path){
+ if(fs.existsSync(path) === false)
+    fs.mkdirSync(path);
+}
 for (const name of thingsToCreate) {
     getDirPath(name);
-    console.log(directoryPath)
+    if(name != ".."){
+        if(name.indexOf(".") > -1){
+            createFile(directoryPath)
+        }else{
+            createDir(directoryPath)
+        }
+    }
     
 }
+
+console.log("files created successfully...")
+
