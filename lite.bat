f46f4aa70a72f2c21bcf63845eb90a1e0b37f255
darshanmarathe <darshanmarathe@gmail.com>
2022-10-06 10:03:52 +0530

    added litexl

---
+++ lite.bat
@@ -0,0 +1,6 @@
+@echo off
+IF [%1]==[.] (
+call C:\ProgramData\chocolatey\lib\lite-xl\tools\lite-xl\lite-xl.exe %cd%
+) ELSE (
+call C:\ProgramData\chocolatey\lib\lite-xl\tools\lite-xl\lite-xl.exe %cd%\%1
+)
