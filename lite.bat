572947d7bc370a873be82f3313d220c99f7e20b3
darshanmarathe <darshanmarathe@gmail.com>
2025-10-24 18:41:57 +0530

    bug: Let the lite go off the cmd

--- lite.bat
+++ lite.bat
@@ -1,7 +1,7 @@
 @echo off
 IF [%1]==[.] (
-call C:\ProgramData\chocolatey\lib\lite-xl\tools\lite-xl\lite-xl.exe %cd%
+call start C:\ProgramData\chocolatey\lib\lite-xl\tools\lite-xl\lite-xl.exe %cd%
 ) ELSE (
-call C:\ProgramData\chocolatey\lib\lite-xl\tools\lite-xl\lite-xl.exe %cd%\%1
+call start C:\ProgramData\chocolatey\lib\lite-xl\tools\lite-xl\lite-xl.exe %cd%\%1
 )
 set BACKDIR=%cd%
